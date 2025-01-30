af2e3288836fa7314f91a16ed5c76dfe55a40f90
DevarshJani <devarshjani0517@gmail>
2025-01-29 19:00:06 -0800

    naive approach

---
+++ RemoveElement.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int size,value;
+    cout<<"Enter the Size of the array";
+    cin>>size;
+
+    vector<int> arr(size);
+    cout<<"Enter the Elements ";
+     for (int i=0 ; i<=size;i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout<<"Enter the Value to be removed";
+    cin>>value;
+
+    for (int i =0 ; i<size;i++)
+    {
+        if(arr[i] == value)
+        {
+            size=size-1;
+          for(int j = i ; j<size;j++)
+          {
+            arr[j] = arr[j+1];
+            
+            break;
+          }
+            
+        }
+    }
+    for (int i=0 ; i<size;i++)
+    {
+        cout << arr[i];
+    }
+}
