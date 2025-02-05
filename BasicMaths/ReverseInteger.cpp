2fa70c0165b23a99f9d82b5856afceb99f35aa41
DevarshJani <devarshjani0517@gmail>
2025-02-04 20:15:33 -0800

    Reverse number for both pos and neg

--- BasicMaths/ReverseInteger.cpp
+++ BasicMaths/ReverseInteger.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int reverseInteger(int num)
 {
     int res = 0,temp;
-    while(num>0)
+    while(num != 0)
     {
-      temp= num%10;
-      res = res*10 + temp;
-      num /= 10;
+      int temp = num%10;
+      //this is the constraint will check number don't overflow
+            if ((res > INT_MAX / 10) || (res < INT_MIN/10))  
+            return 0;
+          res =res*10 + temp;
+          num = num/10;
     }
   return res;
 }
